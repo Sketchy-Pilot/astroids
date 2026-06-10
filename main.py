7abbb81850c6798e180a06ba33dcd419421a6b53
Curtis Limbach <climbach@gmail.com>
2026-06-10 18:43:13 -0500

    added tick

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+    clock = pygame.time.Clock()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0.0
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
